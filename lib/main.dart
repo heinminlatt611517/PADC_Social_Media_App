0223749b83178be7cae171c0944be28c689b9e55
heinminlatt <heinminlatt.dev1@gmail.com>
2024-07-22 11:03:54 +0700

    latest code

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:firebase_installations/firebase_installations.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_app/data/models/authentication_model_impl.dart';
 import 'package:social_media_app/fcm/fcm_service.dart';
-import 'package:social_media_app/fcm/fcm_token_generation.dart';
 import 'package:social_media_app/pages/login_page.dart';
 import 'package:social_media_app/pages/new_feed_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  debugPrint("FCMServerKey:::${await FirebaseAccessToken().getToken()}");
+  //debugPrint("FCMServerKey:::${await FirebaseAccessToken().getToken()}");
   FCMService().listenForMessages();
 
   var firebaseInstallationId =
